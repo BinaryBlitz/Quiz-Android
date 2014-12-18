6e1a509b49df51f8a6a61034c7608852ac9ea677
Pavel <pskozemirov@gmail.com>
2014-12-18 18:30:14 +0300

    Added a timer

--- app/src/main/java/com/quiz/pavel/quiz/controller/TestFragment.java
+++ app/src/main/java/com/quiz/pavel/quiz/controller/TestFragment.java
@@ -1,17 +1,23 @@
 package com.quiz.pavel.quiz.controller;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.quiz.pavel.quiz.R;
 import com.quiz.pavel.quiz.model.Question;
 import com.quiz.pavel.quiz.model.Session;
+import com.quiz.pavel.quiz.model.SessionManager;
 import com.quiz.pavel.quiz.model.SessionQuestion;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -25,18 +31,19 @@ import static com.quiz.pavel.quiz.R.id.variant_a_button;
 public class TestFragment extends Fragment {
 
 
-    @InjectView(variant_a_button) Button mVariantA;
+    @InjectView(R.id.variant_a_button) Button mVariantA;
     @InjectView(R.id.variant_b_button) Button mVariantB;
     @InjectView(R.id.variant_c_button) Button mVariantC;
     @InjectView(R.id.variant_d_button) Button mVariantD;
 
-    private TextView mQuestionTextView;
+    @InjectView(R.id.question_text_view) TextView mQuestionTextView;
+    @InjectView(R.id.timer_textView) TextView mTimerTextView;
 
 
 
-
-    public Session mSession;
+    public SessionManager mSessionManager;
     private SessionQuestion mSessionQuestion;
+
 
 
 
@@ -56,42 +63,23 @@ public class TestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_test, parent, false);
-
-
         ButterKnife.inject(this,v);
-        mQuestionTextView = (TextView)v.findViewById(R.id.question_text_view);
 
-        Question demo = new Question();
-        mQuestionTextView.setText(demo.getText());
+        mSessionManager = SessionManager.newInstance();
+
+        update();
 
 
 
-        mSession = Session.newInstance();
-
-        mSessionQuestion = mSession.getSessionQuestion();
-        mQuestionTextView.setText(mSessionQuestion.getText());
-        String[] vars = mSessionQuestion.getQuestion().getVariants();
-
-        mVariantA.setText(vars[0]);
-        mVariantB.setText(vars[1]);
-        mVariantC.setText(vars[2]);
-        mVariantD.setText(vars[3]);
-
-        mVariantA.setOnClickListener(new View.OnClickListener() {
+        myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
             @Override
-            public void onClick(View view) {
-                mSessionQuestion = mSession.getSessionQuestion();
-                if(mSessionQuestion != null) {
-                    mQuestionTextView.setText(mSessionQuestion.getText());
-                }
-                String[] vars = mSessionQuestion.getQuestion().getVariants();
-
-                mVariantA.setText(vars[0]);
-                mVariantB.setText(vars[1]);
-                mVariantC.setText(vars[2]);
-                mVariantD.setText(vars[3]);
+            public void run() {
+                i++;
+                myHandler.post(myRunnable);
             }
-        });
+        }, 0, 1000);
+
 
 
 
@@ -99,6 +87,70 @@ public class TestFragment extends Fragment {
         return v;
     }
 
+    private int i;
+    Handler myHandler = new Handler();
+    Timer myTimer;
+
+
+
+
+
+
+    final Runnable myRunnable = new Runnable() {
+        public void run() {
+            if(i >= 10){
+                update();
+            }
+            mTimerTextView.setText(String.valueOf(10 - i));
+        }
+    };
+
+
+    /**
+    * Update question and variants of answer
+     */
+    private void update(){
+        i = 0;
+        myHandler.post(myRunnable);
+        Question newQuestion = null;
+        try {
+            newQuestion = mSessionManager.getCurrentQuestion();
+        }catch(Exception ex){
+            getActivity().finish();
+            return;
+        }
+
+        mQuestionTextView.setText(newQuestion.getText());
+
+        String[] vars = newQuestion.getAnswers();
+        mVariantA.setText(vars[0]);
+        mVariantB.setText(vars[1]);
+        mVariantC.setText(vars[2]);
+        mVariantD.setText(vars[3]);
+    }
+
+
+
+
+    @OnClick(R.id.variant_a_button)
+    public void onButtonAClick() {
+        update();
+    }
+
+    @OnClick(R.id.variant_b_button)
+    public void onButtonBClick() {
+        update();
+    }
+
+    @OnClick(R.id.variant_c_button)
+    public void onButtonCClick() {
+        update();
+    }
+
+    @OnClick(R.id.variant_d_button)
+    public void onButtonDClick() {
+        update();
+    }
 
 
 
