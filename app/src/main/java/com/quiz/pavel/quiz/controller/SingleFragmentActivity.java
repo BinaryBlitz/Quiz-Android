1b73cbaa3411a34d958b6b82944df31fe9ba057e
kozemirov <pskozemirov@gmail.com>
2015-03-05 23:32:58 +0300

    Add fullscreen mode during game

--- app/src/main/java/com/quiz/pavel/quiz/controller/SingleFragmentActivity.java
+++ app/src/main/java/com/quiz/pavel/quiz/controller/SingleFragmentActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.quiz.pavel.quiz.R;
 
@@ -14,6 +16,8 @@ public class SingleFragmentActivity extends FragmentActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
--- app/src/main/java/com/quiz/pavel/quiz/model/Theme.java
+++
@@ -1,7 +0,0 @@
-package com.quiz.pavel.quiz.model;
-
-/**
- * Created by pavelkozemirov on 15.02.15.
- */
-public class Theme {
-}
