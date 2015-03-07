f24f2981420181452c989ef72cc60cca2d3ef5e5
kozemirov <pskozemirov@gmail.com>
2015-03-07 12:12:09 +0300

    Use fragments in profileActivity, not perfect

--- app/src/main/java/com/quiz/pavel/quiz/controller/ProfileActivity.java
+++ app/src/main/java/com/quiz/pavel/quiz/controller/ProfileActivity.java
@@ -5,22 +5,40 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
 
 import com.quiz.pavel.quiz.R;
+import com.quiz.pavel.quiz.model.Mine;
+import com.quiz.pavel.quiz.model.PlayerProfile;
 
 /**
  * Created by pavelkozemirov on 15.02.15.
  */
-public class ProfileActivity extends FragmentActivity
+public class ProfileActivity extends ActionBarActivity
         implements ProfileFragment.OnAddNewFragmentCallback{
 
+    private static final String TAG = "ProfileFragment";
+
+    ActionBar mActionBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_fragment);
 
-        FragmentManager fm = getSupportFragmentManager();
+        mActionBar = getSupportActionBar();
+        mActionBar.setTitle(Mine.getInstance(this).getName());
+
+
+        fm = getSupportFragmentManager();
+
+
 
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
@@ -30,21 +48,61 @@ public class ProfileActivity extends FragmentActivity
                     .add(R.id.fragmentContainer, fragment)
                     .commit();
         }
+
+        fm.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                update();
+                method();
+            }
+        });
     }
 
+    private void method() {
+        currentFragment = (ProfileFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
+        mActionBar.setTitle(currentFragment.pp.getName());
+        Log.d(TAG, "ONBACKSTACKCHANGEDLISTENER");
+    }
 
+    FragmentManager fm;
+
+    ProfileFragment currentFragment;
+    FragmentTransaction ft;
 
     @Override
-    public void addFragment(int position) {
-        FragmentManager fm = getSupportFragmentManager();
+    public void addFragment(PlayerProfile player) {
+//        FragmentManager fm = getSupportFragmentManager();
+        mActionBar.setTitle(player.getName());
 
         ProfileFragment fragment = new ProfileFragment();
+        fragment.pp = player;
 
-        FragmentTransaction ft = fm.beginTransaction();
+        ft = fm.beginTransaction();
         ft.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
-        ft.add(R.id.fragmentContainer, fragment);
+        ft.replace(R.id.fragmentContainer, fragment);
 
         ft.addToBackStack(null);
         ft.commit();
     }
+
+    private void update() {
+        if(fm.getBackStackEntryCount() == 0) {
+            Log.d(TAG, "getBackStackentrycount = 0");
+            mActionBar.setDisplayHomeAsUpEnabled(false);
+            mActionBar.setTitle(Mine.getInstance(this).getName());
+        } else {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public void removeFragment() {
+        fm.popBackStack();
+    }
+
+    @Override
+    public void setTitle(String str) {
+    }
+
+
 }
